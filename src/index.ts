df99659196a1a9ed2e963f26e3562785a58f2ca8
brunojacobs1 <brunojacobsschulz@gmail.com>
2020-12-17 09:56:43 -0500

    preparando static files

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import cors from 'cors';
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(express.static('build'));
 
 app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
